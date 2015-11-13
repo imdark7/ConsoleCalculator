7fb5eed370cfc5da067426e372a664fda7685efc
imdark7 <imdark@yandex.ru>
2015-11-13 17:06:30 +0500

    Scanner
    Scanner returned and replaced all int to a double

--- ConsoleCalculator.java
+++ ConsoleCalculator.java
@@ -9,57 +9,54 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class ConsoleCalculator {
-    public static void main(String[] args) throws IOException {
-        /*
-    public static void main(String[] args)
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
         switch(input) {
             case "сложить":
                 System.out.println("Введите первое число:");
-                String inputSum1 = scanner.nextLine();
-                int sum = Integer.parseInt(inputSum1);
+                double inputSum1 = scanner.nextDouble();
                 System.out.println("Введите второе число");
-                String inputSum2 = scanner.nextLine();
-                sum = sum + Integer.parseInt(inputSum2);
+                double inputSum2 = scanner.nextDouble();
+                double sum = inputSum1 + inputSum2;
                 System.out.println("Сумма равна: " + sum);
                 break;
 
             case "умножить":
                 System.out.println("Введите первое число:");
-                String inputMult1 = scanner.nextLine();
-                int mult = Integer.parseInt(inputMult1);
+                double inputMult1 = scanner.nextDouble();
                 System.out.println("Введите второе число");
-                String inputMult2 = scanner.nextLine();
-                mult = mult * Integer.parseInt(inputMult2);
+                double inputMult2 = scanner.nextDouble();
+                double mult = inputMult1 * inputMult2;
                 System.out.println("Произведение равно: " + mult);
                 break;
 
             case "делить":
                 System.out.println("Введите делимое:");
-                String inputDiv1 = scanner.nextLine();
-                double div = Integer.parseInt(inputDiv1);
+                double inputDiv1 = scanner.nextDouble();
                 System.out.println("Введите делитель");
-                String inputDiv2 = scanner.nextLine();
-                div = div / Integer.parseInt(inputDiv2);
+                double inputDiv2 = scanner.nextDouble();
+                double div = inputDiv1 / inputDiv2;
                 System.out.println("Частное равно: " + div);
                 break;
 
             case "вычесть":
                 System.out.println("Введите уменьшаемое число:");
-                String inputSub1 = scanner.nextLine();
-                int sub = Integer.parseInt(inputSub1);
+                double inputSub1 = scanner.nextDouble();
                 System.out.println("Введите вычитаемое число");
-                String inputSub2 = scanner.nextLine();
-                sub = sub - Integer.parseInt(inputSub2);
+                double inputSub2 = scanner.nextDouble();
+                double sub = inputSub1 - inputSub2;
                 System.out.println("Разность равна: " + sub);
                 break;
 
             default:
                 System.out.println("Доступные команды: сложить, вычесть, умножить, делить");
                 break;
-        */
+
+            /*
+
+    public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String input = reader.readLine();
 
@@ -107,6 +104,7 @@ public class ConsoleCalculator {
             default:
                 System.out.println("Доступные команды: сложить, вычесть, умножить, делить");
                 break;
+                 */
         }
     }
 }
